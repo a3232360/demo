be52d643320099a71a8d12d3e556c4dcc9f67440
a3232360 <472431522@qq.com>
2025-01-13 23:09:24 +0800

    Update opl.sh

--- opl.sh
+++ opl.sh
@@ -56,9 +56,9 @@ echo "正在从 GitHub 克隆 opl 仓库..."
 rm -rf /root/opl
 git clone https://github.com/a154225859/opl.git
 
-mkdir -p /root/opl/.config
+mkdir -p /root/opl
 
-echo "$json_param" > /root/opl/.config/keystore.json
+echo "$json_param" > /root/opl/keystore.json
 
 cd opl
 
